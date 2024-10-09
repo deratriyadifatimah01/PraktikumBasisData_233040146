5859c56f88708f0a43c5e782a5f41b96dbbd9318
deratriyadifatimah01 <147587092+deratriyadifatimah01@users.noreply.github.com>
2024-10-09 13:56:40 +0700

    Add files via upload

---
+++ Latihan 1.sql
@@ -0,0 +1,11 @@
+-- Menambahkan Data Ke Table 
+INSERT INTO Mahasiswa (ID, NIM, Nama, Jurusan, TanggalLahir)
+VALUES (22,233040134, 'Fatimah', 'Teknik Informatika', '2006-07-01'),
+(23,233040001, 'Dia', 'Teknik Informatika', '2004-12-11'),
+(24,233040118, 'Anandita', 'Teknik Informatika', '2004-12-04'),
+(25,233040120, 'Dini', 'Teknik Informatika', '2005-11-25'),
+(26,233040122, 'Ilona', 'Teknik Informatika', '2005-07-20'),
+(27,233040124, 'Milda', 'Teknik Informatika', '2005-10-05');
+
+SELECT *
+FROM [dbo].Mahasiswa
